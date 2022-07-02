a8818f1f4234a273193f64d5eec22e22846e3bd7
poojan010 <pdbhatt010@gmail.com>
2022-07-02 14:01:06 +0530

    again init setup

--- chatApp/babel.config.js
+++ chatApp/babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins : [
-    [
-      'module-resolver',
-      {
-        root : ['./src'],
-        alias : {
-            asset : './src/asset',
-            components : './src/components',
-            constanst : './src/constanst',
-            container : './src/container',
-            navigator : './src/navigator',
-            store : './src/store',
-            utils : './src/utils',
-            interfaces : './src/interfaces',
-        }
-      }
-    ]
-  ]
-
 };
